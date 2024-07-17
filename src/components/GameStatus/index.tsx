c2caa9f34e0b8b5b8ebd6454517b138e57dfd940
rmikami <roku.mikami@bitkey.jp>
2024-07-17 13:17:21 +0900

    refactor: GameStatus内の内容を微修正(newGameStatusになっていたのでnewGameStatus()に

--- src/components/GameStatus/index.tsx
+++ src/components/GameStatus/index.tsx
@@ -21,7 +21,7 @@ export function GameStatus({ board, xIsNext }: GameStatusProps): JSX.Element {
       }
     };
 
-    setGameStatus(newGameStatus);
+    setGameStatus(newGameStatus());
   }, [board, xIsNext]);
 
   return (
