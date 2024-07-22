8f06698a4ac14332e89f756540873218c3c777be
rmikami <roku.mikami@bitkey.jp>
2024-07-22 22:17:25 +0900

    docs:前回修正時の残しを微修正

--- src/components/PlayHistory/components/ToggleOrder.tsx
+++ src/components/PlayHistory/components/ToggleOrder.tsx
@@ -1,9 +1,6 @@
 import { ToggleOrderProps } from "../../../type";
 
-export function ToggleOrder({
-  ascending,
-  changeOrder,
-}: ToggleOrderProps) {
+export function ToggleOrder({ ascending, changeOrder }: ToggleOrderProps) {
   function handleOrder(): void {
     changeOrder((prevAscending) => !prevAscending);
   }
