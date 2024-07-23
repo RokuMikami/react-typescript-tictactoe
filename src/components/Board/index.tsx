6e131cca419599ef8c9d9ce83bf45737ffb52905
rmikami <roku.mikami@bitkey.jp>
2024-07-23 16:54:35 +0900

    refactor: existWinnerはGameStatusでも使用していたのでリフトアップし、Boardからは削除

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,10 +1,12 @@
 import { BoardProps } from "../type";
 import { Square } from "./components/square";
-import { calculateWinner } from "../model";
 
-export function Board({ xIsNext, board, onPlay }: BoardProps): JSX.Element {
-  const existWinner: number[][] | null = calculateWinner(board);
-
+export function Board({
+  xIsNext,
+  board,
+  onPlay,
+  existWinner,
+}: BoardProps): JSX.Element {
   function handleClick(i: number, j: number): void {
     if (board[i][j] || existWinner) {
       return;
@@ -18,27 +20,22 @@ export function Board({ xIsNext, board, onPlay }: BoardProps): JSX.Element {
     onPlay(newBoard);
   }
 
-  const boardRows = Array.from({ length: 3 });
-
   return (
     <>
-      {boardRows.map((_, i) => (
+      {board.map((_, i) => (
         <div key={i} className="board-row">
           {board[i].map((square, j) => {
             const keyIndex = j + i * 3;
             const isWinnersSquare =
               existWinner &&
               existWinner.some((elem) => elem[0] === i && elem[1] === j);
-            const squareStyle = isWinnersSquare
-              ? { backgroundColor: "pink" }
-              : {};
 
             return (
               <Square
                 key={keyIndex}
                 value={square}
                 onSquareClick={() => handleClick(i, j)}
-                style={squareStyle}
+                isWinner={isWinnersSquare}
               />
             );
           })}
